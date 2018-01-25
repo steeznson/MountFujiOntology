cd098e26eb738de7f4b1f9f3de16c5794f08aec0
steeznson <james.stevenson09@hotmail.co.uk>
2018-01-25 13:34:19 +0000

    rm'd XML and improved pl script syntax

--- search_ontology.pl
+++ search_ontology.pl
@@ -12,8 +12,6 @@ open (my $fh, '<encoding(UTF-8)', $filename)
 	or die "Could not open file '$filename' $!";
 
 while (my $row = <$fh>) {
-	chomp $row;
-	if ($row =~ $search){
-		print "$row\n";
-	} 
+	chomp $row; 
+	print "$row\n" if ($row =~ $search);
 }
