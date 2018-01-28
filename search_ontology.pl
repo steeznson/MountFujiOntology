3ff0d88c0d8aea5480850dd8d019c2bed8c94d60
steeznson <james.stevenson09@hotmail.co.uk>
2018-01-28 12:12:42 +0000

    added more default search terms to search

--- search_ontology.pl
+++ search_ontology.pl
@@ -3,10 +3,12 @@ use warnings;
 
 my $filename = '36views.owl';
 
-print "View all classes (1) or enter a search term: ";
+print "View all classes (1), all individuals (2),\nall object properties (3) or enter a search term: ";
 chomp (my $search = <STDIN>);
 exit 0 if ($search eq "");
 if ($search eq "1"){$search = "Class:";}
+if ($search eq "2"){$search = "Individual:";}
+if ($search eq "3"){$search = "ObjectProperty:";}
 
 open (my $fh, '<encoding(UTF-8)', $filename)
 	or die "Could not open file '$filename' $!";
