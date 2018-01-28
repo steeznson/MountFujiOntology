ffb19da4cace7267d47e10694d2644093a9b6c20
steeznson <james.stevenson09@hotmail.co.uk>
2018-01-28 11:41:24 +0000

    updated search

--- search_ontology.pl
+++ search_ontology.pl
@@ -3,9 +3,10 @@ use warnings;
 
 my $filename = '36views.owl';
 
-print "Enter search term: ";
+print "View all classes (1) or enter a search term: ";
 chomp (my $search = <STDIN>);
 exit 0 if ($search eq "");
+if ($search eq "1"){$search = "Class:";}
 
 open (my $fh, '<encoding(UTF-8)', $filename)
 	or die "Could not open file '$filename' $!";
